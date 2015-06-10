06cd3774bfdf8c96eb32ce0b3192eaef618687d0
n-gondo123 <n.gondo123@gmail.com>
2015-06-10 20:32:16 +0900

    FizzBuzz2〜6を実装

--- src/main/scala/com/github/gondo/study/gondo/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/gondo/study/gondo/FizzBuzz/FizzBuzz.scala
@@ -5,15 +5,77 @@ package com.github.gondo.study.gondo.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-    (1 to 100).foreach(num => println(fizzBuzz(num)))
+    fizzBuzz1()
+    fizzBuzz2()
+    fizzBuzz3()
+    fizzBuzz4()
+    fizzBuzz5()
+    fizzBuzz6()
   }
 
-  private def fizzBuzz(num: Int): String = {
+  /**
+   * FizzBuzzの判定結果文字列を返す
+   * @param num 整数値
+   * @return 判定結果文字列
+   */
+  private def mkFizzBuzzString(num: Int): String = {
     (num % 3 == 0, num % 5 == 0) match {
       case (true, false) => "Fizz"
       case (false, true) => "Buzz"
       case (true, true) => "FizzBuzz"
       case _ => num.toString
     }
+  }
+
+  /**
+   * FizzBuzzその1(初期バージョン)
+   */
+  private def fizzBuzz1(): Unit = {
+    (1 to 100).foreach(num => println(mkFizzBuzzString(num)))
+  }
+
+  /**
+   * FizzBuzzその2(ワンライナー)
+   */
+  private def fizzBuzz2(): Unit = (1 to 100).map(i => if (i % 15 == 0) "FizzBuzz" else if (i % 5 == 0) "Buzz" else if (i % 3 == 0) "Fizz" else i.toString).foreach(println)
+
+  /**
+   * FizzBuzzその3(2の倍数を除く)
+   */
+  private def fizzBuzz3(): Unit = {
+    (1 to 100).withFilter(_ % 2 != 0).map(mkFizzBuzzString).foreach(println)
+  }
+
+  /**
+   * FizzBuzzその4(カンマ区切り文字列)
+   */
+  private def fizzBuzz4(): Unit = {
+    println((1 to 100).map(mkFizzBuzzString).mkString(","))
+  }
+
+  /**
+   * FizzBuzzその5(Fizz/Buzz/FizzBuzzに該当しない数値の合計)
+   */
+  private def fizzBuzz5(): Unit = {
+    println((1 to 100).filter(i => i % 3 != 0 && i % 5 != 0).sum)
+  }
+
+  /**
+   * num〜endまで再帰処理を行う
+   * @param num 開始値
+   * @param end 終了値
+   */
+  private def recursiveFizzBuzz(num: Int, end: Int): Unit = {
+    if (num <= end ) {
+      println(mkFizzBuzzString(num))
+      recursiveFizzBuzz(num + 1, end)
+    }
+  }
+
+  /**
+   * FizzBuzzその6(イテレーション/for/while 禁止)
+   */
+  private def fizzBuzz6(): Unit = {
+    recursiveFizzBuzz(1, 100)
   }
 }
