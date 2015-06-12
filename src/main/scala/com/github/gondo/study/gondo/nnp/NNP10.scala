4d456f8a3eec4b2b2febc3c3c4270e181d132d65
n-gondo123 <n.gondo123@gmail.com>
2015-06-12 21:23:37 +0900

    NNP 1 - 6を実装

---
+++ src/main/scala/com/github/gondo/study/gondo/nnp/NNP10.scala
@@ -0,0 +1,50 @@
+package com.github.gondo.study.gondo.nnp
+
+/**
+ * Created by naoki.gondo on 2015/06/12.
+ */
+trait NNP10 {
+
+  // P01 (*) Find the last element of a list.
+  def last(list: List[Int]): Int = {
+    list.last
+  }
+
+  // P02 (*) Find the last but one element of a list.
+  def penultimate(list: List[Int]): Int = {
+    list.init.last
+  }
+
+  def nth(n: Int, list: List[Int]): Int = {
+    list(n)
+  }
+
+  def length(list: List[Int]): Int = {
+    list.size
+  }
+
+  def reverse(list: List[Int]): List[Int] = {
+    list.reverse
+  }
+
+  def isPalindrome(list: List[Int]): Boolean = {
+    list == list.reverse
+  }
+
+  def flatten(nested: List[Any]): List[Any] = {
+    ???
+  }
+
+  def compress(list: List[Symbol]): List[Symbol] = {
+    ???
+  }
+
+  def pack(list: List[Symbol]): List[List[Symbol]] = {
+    ???
+  }
+
+  def encode(list: List[Symbol]): List[(Int, Symbol)] = {
+    ???
+  }
+
+}
